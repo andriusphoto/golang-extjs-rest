d90929b7e16659377867bf72f0805d6bd5f17870
andriusphoto <andrius@webmeistrai.lt>
2017-05-12 13:13:37 +0300

    add application

---
+++ controller.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	routing "github.com/go-ozzo/ozzo-routing"
+
+	r "gopkg.in/gorethink/gorethink.v3"
+)
+
+func Get(c *routing.Context) error {
+	q := c.Get("q").(r.Term)
+	res, err := q.Run(c.Get("session").(r.QueryExecutor))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var rows []interface{}
+	err = res.All(&rows)
+	if err != nil {
+		// error
+	}
+	ret := jsonReturnArray{rows, true}
+
+	json, _ := json.Marshal(ret)
+
+	return c.Write(string(json))
+
+}
+func GetOne(c *routing.Context) error {
+	q := c.Get("q").(r.Term)
+	res, err := q.Get(c.Param("id")).Run(c.Get("session").(r.QueryExecutor))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var rows []interface{}
+	err = res.All(&rows)
+	if err != nil {
+		// error
+	}
+	ret := jsonReturnArray{rows, true}
+	json, _ := json.Marshal(ret)
+
+	return c.Write(string(json))
+
+}
+func Delete(c *routing.Context) error {
+	q := c.Get("q").(r.Term)
+	_, err := q.Get(c.Param("id")).Delete().Run(c.Get("session").(r.QueryExecutor))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var rows []interface{}
+	ret := jsonReturnArray{rows, true}
+	json, _ := json.Marshal(ret)
+
+	return c.Write(string(json))
+
+}
+func Create(c *routing.Context) error {
+	q := c.Get("q").(r.Term)
+	var data interface{}
+	err := c.Read(&data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	ins, err1 := q.Insert(data).RunWrite(c.Get("session").(r.QueryExecutor))
+	if err1 != nil {
+		log.Fatalln(err1)
+	}
+
+	res, err2 := q.Get(ins.GeneratedKeys[0]).Run(c.Get("session").(r.QueryExecutor))
+	if err2 != nil {
+		log.Fatalln(err2)
+	}
+
+	var rows []interface{}
+	err3 := res.All(&rows)
+	if err3 != nil {
+		// error
+	}
+	ret := jsonReturnArray{rows, true}
+	json, _ := json.Marshal(ret)
+
+	return c.Write(string(json))
+
+}
+func Update(c *routing.Context) error {
+	q := c.Get("q").(r.Term)
+	var data interface{}
+	err := c.Read(&data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err1 := q.Get(c.Param("id")).Update(data).RunWrite(c.Get("session").(r.QueryExecutor))
+	if err1 != nil {
+		log.Fatalln(err1)
+	}
+
+	res, err2 := q.Get(c.Param("id")).Run(c.Get("session").(r.QueryExecutor))
+	if err2 != nil {
+		log.Fatalln(err2)
+	}
+
+	var rows []interface{}
+	err3 := res.All(&rows)
+	if err3 != nil {
+		// error
+	}
+	ret := jsonReturnArray{rows, true}
+	json, _ := json.Marshal(ret)
+
+	return c.Write(string(json))
+
+}
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-ozzo/ozzo-routing"
+	"github.com/go-ozzo/ozzo-routing/access"
+	"github.com/go-ozzo/ozzo-routing/cors"
+	"github.com/go-ozzo/ozzo-routing/fault"
+	"github.com/go-ozzo/ozzo-routing/slash"
+
+	r "gopkg.in/gorethink/gorethink.v3"
+)
+
+type jsonReturnArray struct {
+	Data    []interface{} `json:"data"`
+	Success bool          `json:"success"`
+}
+
+func main() {
+	router := routing.New()
+	//options := cors.Options{AllowOrigins: "http://dev.mro.flts.local", AllowCredentials: true, AllowHeaders: "*"}
+	router.Use(
+		// all these handlers are shared by every route
+		access.Logger(log.Printf),
+		slash.Remover(http.StatusMovedPermanently),
+		fault.Recovery(log.Printf),
+		cors.Handler(cors.Options{
+			AllowOrigins: "*",
+			AllowHeaders: "*",
+			AllowMethods: "*",
+		}),
+	)
+
+	api := router.Group("/api")
+	// api.Options("/<table>", conect, useTable, Get)
+	api.Get("/<table>", conect, useTable, Get)
+
+	api.Get("/<table>/<id>", conect, useTable, GetOne)
+	api.Delete("/<table>/<id>", conect, useTable, Delete)
+	api.Post("/<table>", conect, useTable, Create)
+	api.Put("/<table>/<id>", conect, useTable, Update)
+	print("Server started... port:7777")
+	http.Handle("/", router)
+	http.ListenAndServe(":7777", nil)
+
+}
+func conect(c *routing.Context) error {
+
+	session, err := r.Connect(r.ConnectOpts{
+		Address: "localhost",
+	})
+	c.Set("session", session)
+	if err != nil {
+
+		log.Fatalln(err)
+	}
+	return nil
+}
+func useTable(c *routing.Context) error {
+	c.Set("q", r.DB("test").Table(c.Param("table")))
+
+	return nil
+}
